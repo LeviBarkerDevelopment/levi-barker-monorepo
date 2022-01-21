01621355dc54bf6e48ac671863bc12cf8d68f0bf
Levi Barker <Levi.barker@outlook.com>
2022-01-20 21:54:05 -0600

    changes

---
+++ frontend/project-gallery/src/root.component.tsx
@@ -0,0 +1,5 @@
+export default function Root(props) {
+  return <section>
+      List projects with MUI cards.  Get data from Firestore through functions.
+    </section>;
+}
---
+++ frontend/react-starter/src/root.component.tsx
@@ -0,0 +1,7 @@
+export default function Root(props) {
+
+  return <section>{props.name} is mounted! 
+  
+    <a href="/">Go back</a>
+  </section>;
+}
